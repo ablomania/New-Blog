76e51194c06f4f64a7463259c29cfca68391122c
Abel Amarteifio <144604908+ablomania@users.noreply.github.com>
2023-10-05 13:58:33 +0000

    Add files via upload

--- webpages/urls.py
+++ webpages/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('home/', views.index, name='home'),
+    path('/', views.index, name='home'),
     path('read/<int:id>', views.read, name='reader'),
     path('articles/', views.articles, name='articles'),
     path('contact/', views.contact, name='contact'),
