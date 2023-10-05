a29709c2a5f679910548cc0933191a25dd649007
Abel Amarteifio <144604908+ablomania@users.noreply.github.com>
2023-10-05 03:04:56 +0000

    Add files via upload

--- NewBlog/settings.py
+++ NewBlog/settings.py
@@ -78,12 +78,14 @@ WSGI_APPLICATION = 'NewBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'MyBlog',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'NewBlog',
+        # 'USER': 'postgres',
+        # 'PASSWORD': '1234',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432'
     }
 }
 
